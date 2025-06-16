a857387288df2b6931afde8c64110b8aee1b961e
L K R N <artemabramcuk45@gmail.com>
2025-06-16 11:03:44 +0300

    chore: обновил конфиграцию

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
